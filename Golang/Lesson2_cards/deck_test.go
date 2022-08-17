8049450fe8aef0edcc15da3f71dfd71ff1fe5ced
luongquochai <51089030+luongquochai@users.noreply.github.com>
2022-08-18 02:05:44 +0700

    Add new test for saveToDeck and newDeckFromFile

--- Golang/Lesson2_cards/deck_test.go
+++ Golang/Lesson2_cards/deck_test.go
@@ -1,15 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"testing"
 )
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
-	fmt.Println("Test-01: Check if the deck length is correct")
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card is Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card is Four of Clubs, but got %v", d[len(d)-1])
+	}
+
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// cleanup file before testing
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, but got %v", len(loadedDeck))
+	}
+
+	// cleanup file after testing
+	os.Remove("_decktesting")
 }
