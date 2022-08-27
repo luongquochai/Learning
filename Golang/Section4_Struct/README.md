4d856d3f411b4f297fb79480e09c79226cd5dc8f
luongquochai <51089030+luongquochai@users.noreply.github.com>
2022-08-27 14:21:44 +0700

    Update pointer in struct to replace value Update note for this section <README.md>

---
+++ Golang/Section4_Struct/README.md
@@ -0,0 +1,3 @@
+NOTE in section 4:
+- &variable: Give me the memory address of the value this variable is poiting at
+- *pointer: Give me the value this memory address is pointing at
--- Golang/Section4_Struct/main.go
+++ Golang/Section4_Struct/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
@@ -18,9 +24,38 @@ func main() {
 	// fmt.Println(Hai)
 	// =========================================
 	// Third way:
-	var Hai person
-	Hai.firstName = "Hai"
-	Hai.lastName = "Luong"
-	fmt.Println(Hai)
-	fmt.Printf("%+v", Hai)
+	// var Hai person
+	// Hai.firstName = "Hai"
+	// Hai.lastName = "Luong"
+	// fmt.Println(Hai)
+	// fmt.Printf("%+v", Hai)
+
+	/* Embedded Struct */
+	Hai := person{
+		firstName: "Hai",
+		lastName:  "Luong",
+		contact: contactInfo{
+			email:   "hai@gmail.com",
+			zipCode: 70000,
+		},
+	}
+
+	HaiPointer := &Hai
+	HaiPointer.updateName("Haiii")
+	Hai.print()
+
+}
+
+/*
+NOTE:
+*person: this is a type description, it means we're working with a pointer to a person
+*pointerToPerson: this is an operator, it means we want to manipute the value the pointer is referencing
+*/
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
