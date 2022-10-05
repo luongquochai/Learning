2a68f51eac8a787f2b03fb6f58aa1e9cd91c93a7
luongquochai <51089030+luongquochai@users.noreply.github.com>
2022-10-05 22:51:09 +0700

    Complete this section

--- Golang/Section6_interface/http/main.go
+++ Golang/Section6_interface/http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -13,6 +16,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	lw := logWriter{}
 
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("How many bytes: ", len(bs))
+	return len(bs), nil
 }
